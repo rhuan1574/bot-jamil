0155e9fba4478cb2cac43f18e0c2b1d0511a29d6
rhuan1574 <rhuanmarques95@gmail.com>
2025-05-14 14:41:27 -0300

    Update Player.js

--- database/models/Player.js
+++ database/models/Player.js
@@ -4,7 +4,17 @@ const playerSchema = new mongoose.Schema({
     discordId: { type: String, required: true, unique: true },
     username: { type: String, required: true },
     metGoal: { type: Boolean, default: false },
-    lastChecked: { type: Date, default: Date.now }
+    lastChecked: { type: Date, default: Date.now },
+    plastico: { type: Number, default: 0 },
+    seda: { type: Number, default: 0 },
+    folha: { type: Number, default: 0 },
+    cascaSemente: { type: Number, default: 0 },
+    dinheiro: { type: Number, default: 0 },
+    isencaoAte: { type: Date },
+    lastReset: { type: Date },
+    endereco: { type: String }, // Farm address
+    contato: { type: String }, // Contact phone number
+    horario: { type: String } // Operating hours
 });
 
 module.exports = mongoose.model('Player', playerSchema);
