a16823efb55925b3ff41db7022fd5e35ade6c668
rhuan1574 <rhuanmarques95@gmail.com>
2025-05-19 23:11:39 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -61,7 +61,7 @@ module.exports = {
             });
 
             // Notificação ajustada para 11:31 AM (UTC-3 = 14:31 UTC)
-            schedule.scheduleJob('00 20 * * *', async function() {
+            schedule.scheduleJob('00 20 * * 1-6', async function() {
                 try {
                     const today = new Date();
                     today.setHours(0, 0, 0, 0); // Início do dia
